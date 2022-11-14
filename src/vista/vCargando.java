b1b070635818ebcb0300f0a77ca70ff8ec375986
Viridiana Leal <lerv060816mhglmra8@soycecytem.mx>
2022-11-14 10:22:11 -0600

    Cargar y password...

---
+++ src/vista/vCargando.java
@@ -0,0 +1,71 @@
+package vista;
+
+import java.awt.EventQueue;
+import java.util.Iterator;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JProgressBar;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+
+public class vCargando extends JFrame {
+
+	private JPanel contentPane;
+	private JProgressBar barCargando = new JProgressBar();
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					vCargando frame = new vCargando();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public void Cargar() {
+		Thread hilo = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for (int i = 0; i <= 100; i++) {
+					barCargando.setValue(i);
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					if(i==100) {
+						JOptionPane.showMessageDialog(null,"YA TERMINO");
+						System.exit(0);
+					}
+				}
+			}
+		});
+		hilo.start();
+	}
+
+	public vCargando() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 93);
+		contentPane = new JPanel();
+		this.setUndecorated(true);
+		this.setLocationRelativeTo(null);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		barCargando.setForeground(new Color(0, 0, 0));
+		barCargando.setStringPainted(true);
+		barCargando.setBounds(-10, 0, 450, 93);
+		contentPane.add(barCargando);
+		Cargar();
+	}
+}
---
+++ src/vista/vLogin.java
@@ -0,0 +1,119 @@
+package vista;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JPasswordField;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class vLogin extends JFrame {
+
+	private JPanel contentPane;
+	private JButton btnEntrar;
+	private JButton btnCancelar;
+	private JPasswordField txtPassword;
+	private JTextField txtUser;
+	private JLabel lblLogo;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					vLogin frame = new vLogin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public vLogin() {
+		getContentPane().setLayout(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 494, 279);
+		contentPane = new JPanel();
+		//this.setUndecorated(true);
+		this.setLocationRelativeTo(null);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Usuario:");
+		lblNewLabel.setBounds(37, 23, 67, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setBounds(37, 94, 86, 20);
+		contentPane.add(lblPassword);
+		
+		btnEntrar = new JButton("Entrar");
+		btnEntrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String usuario=txtUser.getText();
+				String password=String.valueOf(txtPassword.getPassword());
+				JOptionPane.showMessageDialog(null, "Usuario:"+usuario+"\nPassword: " +password);
+			}
+		});
+		btnEntrar.setBounds(37, 175, 89, 23);
+		contentPane.add(btnEntrar);
+		
+		btnCancelar = new JButton("Cancel");
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "ADIOS");
+				System.exit(0);
+			}
+		});
+		btnCancelar.setBounds(162, 175, 89, 23);
+		contentPane.add(btnCancelar);
+		
+		txtPassword = new JPasswordField();
+		txtPassword.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				if(txtPassword.getText().length()>10) {
+					e.consume();
+				}
+			}
+		});
+		txtPassword.setText("");
+		txtPassword.setBounds(47, 125, 194, 20);
+		contentPane.add(txtPassword);
+		
+		txtUser = new JTextField();
+		txtUser.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				if(txtUser.getText().length()>10) {
+					e.consume();
+			}
+			}
+		});
+		txtUser.setBounds(47, 48, 198, 20);
+		contentPane.add(txtUser);
+		txtUser.setColumns(10);
+		
+		lblLogo = new JLabel("New label");
+		lblLogo.setIcon(new ImageIcon(vLogin.class.getResource("/img/preparatoria.png")));
+		lblLogo.setBounds(287, 51, 123, 131);
+		contentPane.add(lblLogo);
+	}
+}
